a363b66c2b13ec0175c60040a455cafd2c1b8256
Gautam Sarkar <gautam.sarkar3333@gmail.com>
2023-12-30 09:29:25 +0530

    feat: add Contact component

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import Layout from "./Layout";
 import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+
+import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ path: "", element: <Home /> }],
+    children: [
+      { path: "", element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "contact", element: <Contact /> },
+    ],
   },
 ]);
 
